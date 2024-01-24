47c4c7b8ffb0ed7cd9c6f32b851bf75840d14462
morteza22333 <morteza.mk14@gmail.com>
2024-01-24 11:35:51 +0330

    init

---
+++ main.py
@@ -0,0 +1,52 @@
+
+import numpy as np
+from pandas import DataFrame
+
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+import os
+import io
+
+os.system("cls")
+
+data = DataFrame({'message': [], 'class': []})
+
+# _____________این فانکشن تمامی فایل های کوجود را استخرااج میکند و هر خط از فایل  ها را در ارایه اضافه میکند
+def read_files(directorypath,classification):
+    data = []
+    index=[]
+    for direc in os.listdir(directorypath):
+        path = directorypath +"/"+ direc
+        index.append(path)
+        file = open(path, "r",encoding="utf8",errors="ignore")
+        in_body=False
+        for line in file.readlines():
+            if in_body:
+                if (line != None and line != "\n"):
+                    data.append({"message":line,"class":classification})
+            elif line == "\n":
+                in_body = True
+        file.close()
+    return DataFrame(data)
+
+# ________ایمیل های spam شده را در یه ارایه میریزد
+spam_data=read_files("./emails/spam","spam")
+# ________ایمیل های اسپم نشده را در یک ارایه میریزد
+not_spam_data= read_files("./emails/not_spam/","not_spam")
+data=data.append(spam_data)
+data=data.append(not_spam_data)
+vectorizer=CountVectorizer()
+# _____این تابع به ما میگویید هر کلمه چند بار تکرار شده و کلمات مهم را پیدا میکند
+count=vectorizer.fit_transform(data["message"].values)
+target=data['class'].values
+# ______در اینجا مدل را اموزش میدهیم و پارامتر دوم لیبل های ما است که دو مورد بیشتر نیست اسپم و نات اسپم
+classifire=MultinomialNB()
+classifire.fit(count,target)
+# در اینجا دیتای ازمایشی درست میکنیم تا مدل را تست کنیم
+example=["buy free","how are you"]
+example_count=vectorizer.transform(example)
+
+# در اینجا مدل را ازمایش میکنیم
+predict=classifire.predict(example_count)
+
+print(predict)
